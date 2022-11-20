310bc68f6ccf9aad17344288be8bf28047fd81fd
zhaokang <johnson_90s@163.com>
2022-11-20 22:30:15 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
+        changeOrigin: true
       },
     },
   },
